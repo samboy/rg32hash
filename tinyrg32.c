9ec143c91147bd8dade60e50560157ddb5c7a4cf
Sam Trenholme <strenholme.usenet@gmail.com>
2015-06-29 22:24:10 -0700

    Update tinyrg32.c to have binary truncated output, binary stream, or even (now undocumented) stream of hex digits

--- tinyrg32.c
+++ tinyrg32.c
@@ -1,6 +1,6 @@
 #include <stdio.h>    // To run this: ./tinyrg32 '¡Es una niña linda!'
-#include <stdint.h>   // Or ./tinyrg32 --binary-stream '¡Es una niña!'
-#define rgp uint32_t  // Or even ./tinyrg32 --hex --stream '¡La niña!'
+#include <stdint.h>   // Or ./tinyrg32 --binary-output '¡Es una niña!'
+#define rgp uint32_t  // Or even ./tinyrg32 --binary --stream '¡Niña!'
 #define rgf(a) for(c=0;c<a;c++){ // Tiny RadioGatun[32] implementation
 #define rgn w[c*13]^=s;u[16+c]^=s;      // Public domain - NO WARRANTY
 void rgk(rgp*a,rgp*b){rgp m=19,A[19],x,o=13,c,y,r=0,q[3];rgf(3)for(q[c
@@ -11,5 +11,5 @@ a^=1;}void rgl(rgp*u,rgp*w,char*v){rgp s,q,c,x;rgf(39)w[c]=u[c%19]=0;}
 for(;;rgk(u,w)){rgf(3)for(s=q=0;q<4;){x=*v++;s|=(x?x&255:1)<<8*q++;if(
 !x){rgn;rgf(17)rgk(u,w);}return;}}rgn;}}}rgp rgi(rgp*m,rgp*b,rgp*a){*a
 &2?rgk(m,b):0;return m[*a^=3];}int main(int g,char**v){rgp b[39],m[19]
-,p=2,c,j;rgl(m,b,v[g-1]);rgf(8)j=rgi(m,b,&p);j=(j<<24|j>>8<<24>>8|j<<8
->>24<<8|j>>24);printf(g&1?"%c":"%08x",j);c-=g>2;}return printf("\n");}
+,p=2,c,j;rgl(m,b,v[--g]);rgf(8)j=rgi(m,b,&p);j=(j<<24|j>>8<<24>>8|j<<8
+>>24<<8|j>>24);printf(g&2?"%c":"%08x",j);c-=g>2;}return printf("\n");}
