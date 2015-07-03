4d76f7aff2cbce36eef7fad122d7b6b0a43f75dd
Sam Trenholme <strenholme.usenet@gmail.com>
2015-07-03 00:06:52 -0700

    tinyrg32.c update: Binary data only available as endless stream; hex data now 32-bit packets on each own line by default unless they do the undocumented ./tinyrg32 --endless --hex --data ‘key’

--- tinyrg32.c
+++ tinyrg32.c
@@ -1,6 +1,6 @@
 #include <stdio.h>     // To run this: ./tinyrg32 '¡Es una niña linda!'
-#include <stdint.h>    // Or ./tinyrg32 --binary-output '¡Es una niña!'
-#define p uint32_t     // Or even ./tinyrg32 --binary --stream '¡Niña!'
+#include <stdint.h>    // Or ./tinyrg32 --binary-stream '¡Es una niña!'
+#define p uint32_t     // Or maybe ./tinyrg32 --hex --data '¡Una niña!'
 #define f(a) for(c=0;c<a;c++)     // Tiny RadioGatun[32] implementation
 #define n w[c*13]^=s;u[16+c]^=s;         // Public domain - NO WARRANTY
 void k(p*a,p*b){p m=19,A[19],x,o=13,c,y,r=0,q[3];f(3){for(q[c]=b[y=c*o+
@@ -10,5 +10,5 @@ c+r)&31;y=c*7%m;x=a[y++];x^=a[y%m]|~a[(y+1)%m];A[c]=x>>r|x<<(32-r);}f(m
 p*u,p*w){p s,q,c,x;f(39)w[c]=u[c%19]=0;for(;;k(u,w)){f(3){for(s=q=0;q<4
 ;){x=*v++;s|=(x?x&255:1)<<8*q++;if(!x){n;f(17)k(u,w);return;}}n;}}}p i(
 p*m,p*b,p*a){*a&2?k(m,b):0;return m[*a^=3];}int main(int g,char**v){p c
-,j,b[39],m[19],q=2,z;l(v[--g],m,b);f(8){j=i(m,b,&q);z=c;f(4){printf(g&2
-?"%c":"%02x",j&255);j>>=8;}c=g>2?1:z;if(g>4||c>6)puts("");}return 0;}
+,j,b[39],m[19],q=2,z;l(v[--g],m,b);f(8){j=i(m,b,&q);z=c;f(4){printf(g==
+2?"%c":"%02x",j&255);j>>=8;}c=g>1?1:z;if(g==3||c>6)puts("");}return 0;}
