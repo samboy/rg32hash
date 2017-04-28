c2c54d49006d60ce1f5e493cbd3b3cacaf4ff968
Sam Trenholme <strenholme.usenet@gmail.com>
2017-04-27 23:42:42 -0700

    tinyapi.c: Four more bytes removed

--- Makefile
+++ Makefile
@@ -13,10 +13,10 @@ rg32-floats: rg32-floats.c
 	cc -O3 -o rg32-floats rg32-floats.c
 
 tinyrg32: tinyrg32.c
-	cc -O3 -o tinyrg32 tinyrg32.c
+	cc -Wall -O3 -o tinyrg32 tinyrg32.c
 
 tinyapi: tinyapi.c
-	cc -O3 -o tinyapi tinyapi.c
+	cc -Wall -O3 -o tinyapi tinyapi.c
 
 rg32-55: rg32-55.c
 	cc -O3 -o rg32-55 rg32-55.c
--- tinyapi.c
+++ tinyapi.c
@@ -1,15 +1,15 @@
 #include <stdint.h> // Public domain crypto-strong random numbers
 #define rgp uint32_t // NO WARRANTY
-#define rgf(a) for(c=0;c<a;c++)
+#define rg(a) for(c=0;c<a;c++)
 #define rgn w[c*13]^=s;u[16+c]^=s;
-void rg(rgp*a,rgp*b){rgp m=19,A[19],x,o=13,c,y,r=0;rgf(12)b[c+c%3*
-o]^=a[c+1];rgf(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1)%m
-];A[c]=x>>r|x<<(32-r);}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y%m]^A[(y
-+1)%m]^A[(y+4)%m];*a^=1;rgf(3)a[c+o]^=b[c*o]=b[c*o+o];}void rgl(
-rgp*u,rgp*w,char*v){rgp s,q,c,x;rgf(40)w[c]=u[c%19]=0;for(;;rg(u
-,w)){rgf(3){for(s=q=0;q<4;){x=*v++;s|=(x?255&x:1)<<8*q++;if(!x){
-rgn;rgf(17)rg(u,w);return;}}rgn;}}}rgp rgi(rgp*m,rgp*b,rgp*a){if
-(*a&2)rg(m,b);return m[*a^=3];}
+void rgf(rgp*a,rgp*b){rgp m=19,A[19],x,o=13,c,y,r=0;rg(12)b[c+c%
+3*o]^=a[c+1];rg(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1
+)%m];A[c]=x>>r|x<<(32-r);}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y%m]
+^A[(y+1)%m]^A[(y+4)%m];*a^=1;rg(3)a[c+o]^=b[c*o]=b[c*o+o];}void
+rgl(rgp*u,rgp*w,char*v){rgp s,q,c,x;rg(40)w[c]=u[c%19]=0;for(;;
+rgf(u,w)){rg(3){for(s=q=0;q<4;){x=*v++;s|=(x?255&x:1)<<8*q++;if
+(!x){rgn;rg(17)rgf(u,w);return;}}rgn;}}}rgp rgi(rgp*m,rgp*b,rgp
+*a){if(*a&2)rgf(m,b);return m[*a^=3];}
 
 /* Example of API usage, non-Golfed (also public domain) */
 #include <stdio.h>
