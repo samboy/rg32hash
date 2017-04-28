ce4e635c53691788fe015f8d746c6f4881d0c37a
Sam Trenholme <strenholme.usenet@gmail.com>
2017-04-28 00:13:11 -0700

    tinyapi.c: Five bytes smaller; right margin even again

--- tinyapi.c
+++ tinyapi.c
@@ -1,15 +1,14 @@
-#include <stdint.h> // Public domain crypto-strong random numbers
-#define rgp uint32_t // NO WARRANTY
-#define rg(a) for(c=0;c<a;c++)
+#include <stdint.h> // Public domain random numbers
+#define rg uint32_t // NO WARRANTY
+#define rgp(a) for(c=0;c<a;c++)
 #define rgn w[c*13]^=s;u[16+c]^=s;
-void rgf(rgp*a,rgp*b){rgp m=19,A[19],x,o=13,c,y,r=0;rg(12)b[c+c%
-3*o]^=a[c+1];rg(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1
-)%m];A[c]=x>>r|x<<(32-r);}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y%m]
-^A[(y+1)%m]^A[(y+4)%m];*a^=1;rg(3)a[c+o]^=b[c*o]=b[c*o+o];}void
-rgl(rgp*u,rgp*w,char*v){rgp s,q,c,x;rg(40)w[c]=u[c%19]=0;for(;;
-rgf(u,w)){rg(3){for(s=q=0;q<4;){x=*v++;s|=(x?255&x:1)<<8*q++;if
-(!x){rgn;rg(17)rgf(u,w);return;}}rgn;}}}rgp rgi(rgp*m,rgp*b,rgp
-*a){if(*a&2)rgf(m,b);return m[*a^=3];}
+void rgf(rg*a,rg*b){rg m=19,A[19],x,o=13,c,y,r=0;rgp(12)b[c+c%3*o]^=a
+[c+1];rgp(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1)%m];A[c]=x
+>>r|x<<(32-r);}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y%m]^A[(y+1)%m]^A[(y
++4)%m];*a^=1;rgp(3)a[c+o]^=b[c*o]=b[c*o+o];}void rgl(rg*u,rg*w,char*v
+){rg s,q,c,x;rgp(40)w[c]=u[c%19]=0;for(;;rgf(u,w)){rgp(3){for(s=q=0;q
+<4;){x=*v++;s|=(x?255&x:1)<<8*q++;if(!x){rgn;rgp(17)rgf(u,w);return;}
+}rgn;}}}rg rgi(rg*m,rg*b,rg*a){if(*a&2)rgf(m,b);return m[*a^=3];}
 
 /* Example of API usage, non-Golfed (also public domain) */
 #include <stdio.h>
