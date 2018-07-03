8c979f95027ee848d7d6dac98b175c87529200a9
Sam Trenholme <strenholme.usenet@gmail.com>
2018-07-02 21:41:13 -0700

    I got nanorg32.c to be under 600 bytes in size! It’s now 580 or 589 bytes in size (depending on whether we make it more narrow for smartphone screens and books).  Some things I did to reduce the size:
    * The input map exclusive or only needs to be in the code once if we run it
      byte-by-byte instead of word-by-word.
    
    * There were a few redundant variables I was able to remove from the code.

--- C/nanorg32-vertical.c
+++ C/nanorg32-vertical.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 #define b(z) for(c=0;c<z;c++)
-#define d f[c*13]^=s;e[16+c]^=s;
-uint32_t c,e[19],f[40],g=19,h=13
-,r,s,t,n[19],i,k;void m(){int c,
-j=0;b(12)f[c+c%3*h]^=e[c+1];b(g)
-{j=(c+j)&31;i=c*7%g;k=e[i++];k^=
-e[i%g]|~e[(i+1)%g];n[c]=k>>j|k<<
-(32-j);}for(i=39;i--;f[i+1]=f[i]
-)e[i%g]=n[i%g]^n[(i+1)%g]^n[(i+4
-)%g];*e^=1;b(3)e[c+h]^=f[c*h]=f[
-c*h+h];}int main(int p,char**v){
-char *q=v[--p];b(40)f[c]=e[c%19]
-=0;for(;;m()){b(3){for(s=r=0;r<4
-;){t=*q++;s|=(t?t&255:1)<<8*r++;
-if(!t){d;b(17)m();t=2;b(8){if(t&
-2)m();r=c;s=e[t^=3];b(4){printf(
-"%02x",s&255);s>>=8;}c=r;}return
-puts("");}}d;}}}
+uint32_t c,e[19],f[40],g=19,h
+=13,n[19],i,k;void m(){int c,
+j=0;b(12)f[c+c%3*h]^=e[c+1];b
+(g){i=c*7%g;k=e[i++];j=(c+j)&
+31;k^=e[i%g]|~e[(i+1)%g];n[c]
+=k>>j|k<<(32-j);}for(i=39;i--
+;f[i+1]=f[i])e[i%g]=n[i%g]^n[
+(i+1)%g]^n[(i+4)%g];b(3)e[c+h
+]^=f[c*h]=f[c*h+h];*e^=1;}int
+main(int p,char**v){char *q=v
+[--p];b(40)f[c]=e[c%g]=0;for(
+;;m()){b(3){for(p=0;p<4;){i=*
+q++;f[c*13]^=k=(i?255&i:1)<<8
+*p++;e[16+c]^=k;if(!i){b(17)m
+();p=2;b(8){if(p&2)m();i=c;k=
+e[p^=3];b(4){printf("%02x",k&
+255);k>>=8;}c=i;}return puts(
+"");}}}}}
