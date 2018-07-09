95b9dd1da082a2187631e24b44713570469764d7
Sam Trenholme <strenholme.usenet@gmail.com>
2018-07-08 23:37:20 -0700

    nanorg32-vertical.c 586 → 584 bytes by using 84 more bytes of memory

--- C/nanorg32-vertical.c
+++ C/nanorg32-vertical.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #define b(z) for(c=0;c<z;c++)
-uint32_t c,e[19],f[40],g=19,h
+uint32_t c,e[40],f[40],g=19,h
 =13,n[19],i,k;void m(){int c,
 j=0;b(12)f[c+c%3*h]^=e[c+1];b
 (g){i=c*7%g;k=e[i++];j=(c+j)&
@@ -11,11 +11,11 @@ j=0;b(12)f[c+c%3*h]^=e[c+1];b
 (i+1)%g]^n[(i+4)%g];b(3)e[c+h
 ]^=f[c*h]=f[c*h+h];*e^=1;}int
 main(int p,char**v){char *q=v
-[--p];b(40)f[c]=e[c%g]=0;for(
-;;m()){b(3){for(p=0;p<4;){f[c
-*h]^=k=(*q?255&*q:1)<<8*p++;e
-[16+c]^=k;if(!*q++){b(17)m();
-p=2;b(8){if(p&2)m();i=c;k=e[p
-^=3];b(4){printf("%02x",k&255
-);k>>=8;}c=i;}return puts("")
-;}}}}}
+[--p];b(40)f[c]=e[c]=0;for(;;
+m()){b(3){for(p=0;p<4;){f[c*h
+]^=k=(*q?255&*q:1)<<8*p++;e[c
++16]^=k;if(!*q++){b(17)m();p=
+2;b(8){if(p&2)m();k=e[p^=3];i
+=c;b(4){printf("%02x",k&255);
+k>>=8;}c=i;}return puts("");}
+}}}}
