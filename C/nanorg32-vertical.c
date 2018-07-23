7ded2bac018b1c047beb13e85655b187c2a85098
Sam Trenholme <strenholme.usenet@gmail.com>
2018-07-23 00:18:02 -0700

    nanorg32-vertical.c: 564 bytes

--- C/nanorg32-vertical.c
+++ C/nanorg32-vertical.c
@@ -9,12 +9,12 @@ j=0;b(12)f[c+c%3*h]^=e[c+1];b
 =k>>j|k<<(32-j);}for(i=39;i--
 ;f[i+1]=f[i])e[i]=n[i]^n[(i+1
 )%g]^n[(i+4)%g];b(3)e[c+h]^=f
-[c*h]=f[c*h+h];*e^=1;}/*:-)*/
-int main(int p,char**v){char*
-q=v[--p];for(;;m()){b(3){for(
-p=0;p<4;){f[c*h]^=k=(*q?255&*
-q:1)<<8*p++;e[c+16]^=k;if(!*q
-++){b(17)m();p=2;b(8){if(p&2)
-m();k=e[p^=3];i=c;b(4){printf
-("%02x",k&255);k>>=8;}c=i;}//
-return puts("");}}}}}
+[c*h]=f[c*h+h];/**/*e^=1;}int
+main(int p,char**v){char*q=v[
+--p];for(;;m()){b(3){for(p=0;
+p<4;){f[c*h]^=k=(*q?255&*q:1)
+<<8*p++;e[c+16]^=k;if(!*q++){
+b(17){m();}p=2;b(8){if(p&2)m(
+);k=e[p^=3];for(i=4;i--;k>>=8
+)printf("%02x",k&255);}return
+puts("");}}}}}
