17662f694ae66ec8cd4fa6cbc8115caff51d3e88
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-13 16:35:26 -0700

    Clang nuttyness: Workaround for Clang bug
    Yes, I am aware that page 84 of
    http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1124.pdf states that
    "If the value of the right [bitwise shift] operand is negative or is
    greater than or equal to the width of the promoted left operand, the
    behavior is undefined."
    
    However, GCC and TCC do the right thing here (foo<<32 should be 0 if
    foo is a 32-bit unsigned int); why not Clang at higher optimization
    levels?

--- C/nanorg32-vertical.c
+++ C/nanorg32-vertical.c
@@ -6,15 +6,15 @@ uint32_t c,e[42],f[42],g=19,h
 j=0;b(12)f[c+c%3*h]^=e[c+1];b
 (g){i=c*7%g;k=e[i++];j=(c+j)&
 31;k^=e[i%g]|~e[(i+1)%g];n[c]
-=n[c+g]=k>>j|k<<(32-j);}for(i
-=39;i--;f[i+1]=f[i])e[i]=n[i]
-^n[i+1]^n[i+4];b(3)e[c+h]^=f[
-c*h]=f[c*h+h];*e^=1;}int main
-(int p,char**v){char*q=v[--p]
-;for(;;m()){b(3){for(p=0;p<4;
-){f[c*h]^=k=(*q?255&*q:1)<<8*
-p++;e[c+16]^=k;if(!*q++){p=2;
-b(18)m();b(8){k=e[p^=3];for(i
-=4;i--;k>>=8)printf("%02x",k&
-255);if(p&2)m();}return puts(
-"");}}}}}
+=n[c+g]=k>>j|k<<((32-j)&31);}
+for(i=39;i--;f[i+1]=f[i])e[i]
+=n[i]^n[i+1]^n[i+4];b(3)e[c+h
+]^=f[c*h]=f[c*h+h];*e^=1;}int 
+main(int p,char**v){char*q=v[
+--p];for(;;m()){b(3){for(p=0;
+p<4;){f[c*h]^=k=(*q?255&*q:1)
+<<8*p++;e[c+16]^=k;if(!*q++){
+p=2;b(18)m();b(8){k=e[p^=3];i
+=c;b(4){printf("%02x",k&255);
+k>>=8;}c=i;if(p&2)m();}return
+puts("");}}}}}
