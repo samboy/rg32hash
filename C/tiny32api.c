2980995a9be1fbef7d0f5d947c96abb80ef71f55
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-14 08:02:02 -0700

    Updated everything except tinyapi.c to have clang a<<32 bugfix

--- C/tiny32api.c
+++ C/tiny32api.c
@@ -1,12 +1,12 @@
 #include <stdint.h> // Public domain random numbers
 #define rg uint32_t // NO WARRANTY
 #define rgp(a) for(c=0;c<a;c++)
-void rgf(rg*a,rg*b){rg m=19,A[19],x,o=13,c,y,r=0;rgp(12)b[c+c%3*o]^=a
-[c+1];rgp(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1)%m];A[c]=x
->>r|x<<(32-r);}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y%m]^A[(y+1)%m]^A[(y
-+4)%m];*a^=1;rgp(3)a[c+o]^=b[c*o]=b[c*o+o];}void rgl(rg*u,rg*w,rg v){
-rg c;rgp(40)w[c]=u[c%19]=0;*w=u[16]=v;w[13]=u[17]=1;rgp(17)rgf(u,w);}
-rg rgi(rg*m,rg*b,rg*a){if(*a&2)rgf(m,b);return m[*a^=3];}
+void rgf(rg*a,rg*b){rg m=19,A[45],x,o=13,c,y,r=0;rgp(12)b[c+c%3*o]^=a
+[c+1];rgp(m){r=(c+r)&31;y=c*7;x=a[y++%m];x^=a[y%m]|~a[(y+1)%m];A[c]=A
+[c+m]=x>>r|x<<(32-r)%32;}for(y=39;y--;b[y+1]=b[y])a[y%m]=A[y]^A[y+1]^
+A[y+4];*a^=1;rgp(3)a[c+o]^=b[c*o]=b[c*o+o];}void rgl(rg*u,rg*w,rg v){
+rg c;rgp(40)u[c%19]=w[c]=0;*w=u[16]=v;w[13]=u[17]=1;rgp(17)rgf(u,w);}
+rg rgi(rg*m,rg*b,rg*a){if(*a&2){rgf(m,b);}return m[*a^=3];}
 
 /* This version of RadioGatún takes a 32-bit unsigned int as its seed.  This
  * way, we can reduce the code size in applications where we simply use RG32
