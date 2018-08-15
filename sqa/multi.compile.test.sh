f5069ff6ec4c2437460587c5709d81f2cf3251b1
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-15 10:54:33 -0700

    Add -Os flag to multi-compiler test

--- sqa/multi.compile.test.sh
+++ sqa/multi.compile.test.sh
@@ -10,7 +10,7 @@ if [ -z "$PROGRAM" ] ; then
 fi
 
 for cc in gcc clang ; do
-    for opt in '-g' '' '-O1' '-O2' '-O3' ; do
+    for opt in '-g' '' '-Os' '-O1' '-O2' '-O3' ; do
         $cc $opt -o test.exe $PROGRAM
         echo Program $PROGRAM with compiler $cc and flags \'$opt\'
         ./do.test.sh ./test.exe
