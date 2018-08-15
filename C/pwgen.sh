b831a57658e309eb78a11c4af46fbf0044309c5c
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-15 07:32:35 -0700

    tinyrg32.c: Update all copies of tinyrg32 with crash fix.
    I have confirmed, as I suspected when looking at the source, that
    tinyrg32 crashed after generating 4 billion (2 ** 32) 32-bit numbers.
    This bug has now been fixed.  I thinkfully had a couple tricks to add
    the three bytes needed to fix the bug without affecting functionality.

--- C/pwgen.sh
+++ C/pwgen.sh
@@ -79,13 +79,13 @@ if ! command -v tinyrg32 > /dev/null 2>&1 ; then
 #define b(z) for(c=0;c<(z);c++) // ./tinyrg32 --passwords _Q 3 'Xz' | head
 uint32_t c,e[40],f[40],g=19,h=13,r,s,t,n[45],i,k,y,z;void m(){int c,j=0;b(
 12)f[c+c%3*h]^=e[c+1];b(g){j=(c+j)&31;i=c*7%g;k=e[i++];k^=e[i%g]|~e[(i+1)%
-g];n[c]=n[c+g]=k>>j|k<<((32-j)&31);}for(c=39;c--;f[c+1]=f[c])e[c]=n[c]^n[c
-+1]^n[c+4];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];}int main(int p,char**v){char
-*q=v[--p],*x=0;for(;;m()){b(3){for(r=0;r<4;){f[c*h]^=k=(*q?*q&255:1)<<r++*
-8;e[16+c]^=k;if(!*q++){b(17)m();b(p<3?8:89*p){if(~t&1)m();s=e[(t&1)+1];r=(
-p&3)-2?c:1;b(4){i=s;if(p&4){x=v[p-2];y=z=z?z:*v[p-1]%16;i&=31;i+=i<8?50:89
-;}s>>=8;printf(p==2||p&4?"%c":"%02x",255&i);}if(((++t%8==0||(p&22)==2)&&p-
-2&&!y)||r+1>=89*p){puts("");}c=r;if(!--z)puts(*x==95?x:"");}return 0;}}}}}
+g];n[c]=n[c+g]=k>>j|k<<(32-j)%32;}for(c=39;c--;f[c+1]=f[c])e[c]=n[c]^n[c+1
+]^n[c+4];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];}int main(int p,char**v){char*q
+=v[--p],*x=0;for(;;m()){b(3){for(r=0;r<4;){f[c*h]^=k=(*q?*q&255:1)<<r++*8;
+e[16+c]^=k;if(!*q++){b(17)m();b(p<3?8:89*p){if(~t&1)m();s=e[(t&1)+1];r=(p&
+3)-2?c:1;b(4){i=s;if(p&4){x=v[p-2];y=z=z?z:*v[p-1]%16;i&=31;i+=i<8?50:89;}
+s>>=8;printf(p==2||p&4?"%c":"%02x",255&i);}if(((++t%8==0||(p&22)==2)&&!y&&
+p-2)||r+2>89*p){puts("");}c=r;if(y&&!--z)puts(*x==95?x:"");}return 0;}}}}}
 EOF
 	cc -o tinyrg32-$$ tinyrg32-$$.c
 	TINYRG32=./tinyrg32-$$
