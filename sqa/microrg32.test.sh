2db63413c1a4e4d6d9583cb0af4499c6fdd8bfa4
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-21 20:51:34 -0700

    Add test to make sure microrg32 makes correct base32 rg32 hashes

---
+++ sqa/microrg32.test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PROG="$1"
+
+if [ -z "$PROG" ] ; then
+	echo Usage: $0 {program_name}
+	echo Where the program is microrg32 compiled
+	exit 255
+fi
+
+cat Input | awk '{print "export P='\''" $0 "'\''"; 
+		  print "'${PROG}' @ 8 | head -1"}' | bash | \
+	tr -d '\015' > output.test
+
+cat output.success | ./base32.convert.py > output.base32
+
+if ! cmp output.base32 output.test > /dev/null 2>&1 ; then
+	echo Test failed
+	cat output.8859-1 | ./base32.convert.py > output.base32
+	if cmp output.base32 output.test > /dev/null 2>&1 ; then
+		echo The script is converting UTF-8 strings in to
+		echo ISO 8859-1 strings.  It is otherwise generating
+		echo correct base32 RadioGatun[32] hashes.
+	fi
+	exit 255
+fi
+
+echo Success: Good base32 hashes
+rm output.test output.base32
