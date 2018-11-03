c8002a3c8dfbb77d06c1ab3a320da40031f26b9f
Sam Trenholme <strenholme.usenet@gmail.com>
2018-11-03 06:47:25 -0700

    nanorg32.c: Another size reduction trick

--- C/nanorg32.c
+++ C/nanorg32.c
@@ -6,6 +6,6 @@ uint32_t c,e[40],f[40],g=19,h=13,n[45],i,k;void m(){int c,j=0;b
 (i+1)%g];n[c]=n[c+g]=k>>j%32|k<<(32-j)%32;}for(c=39;c--;f[c+1]=
 f[c])e[c]=n[c]^n[c+1]^n[c+4];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];
 }int main(int p,char**v){char*q=v[--p];for(;;m()){b(3){for(p=0;
-p<4;){f[c*h]^=k=(*q?255&*q:1)<<8*p++;e[16+c]^=k;if(!*q++){p=2;b
-(17)m();b(8){if(p&2)m();i=c;k=e[p^=3];b(4){printf("%02x",255&k)
-;k>>=8;}c=i;}return 0&puts("");}}}}}
+p<4;){f[c*h]^=k=(*q?255&*q:1)<<8*p++;e[16+c]^=k;if(!*q++){b(18)
+m();b(8){i=c;k=e[1+c%2];b(4){printf("%02x",255&k);k>>=8;}c=i;if
+(c%2)m();}return 0&puts("");}}}}}
