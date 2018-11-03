ca276b606cdf85d5275e6f85bba730451a015294
Sam Trenholme <strenholme.usenet@gmail.com>
2018-11-03 06:43:17 -0700

    nanorg32.c bugfix: Return 0 to indicate success to scripts

--- C/nanorg32.c
+++ C/nanorg32.c
@@ -8,4 +8,4 @@ f[c])e[c]=n[c]^n[c+1]^n[c+4];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];
 }int main(int p,char**v){char*q=v[--p];for(;;m()){b(3){for(p=0;
 p<4;){f[c*h]^=k=(*q?255&*q:1)<<8*p++;e[16+c]^=k;if(!*q++){p=2;b
 (17)m();b(8){if(p&2)m();i=c;k=e[p^=3];b(4){printf("%02x",255&k)
-;k>>=8;}c=i;}return puts("");}}}}}
+;k>>=8;}c=i;}return 0&puts("");}}}}}
