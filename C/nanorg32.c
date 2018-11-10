cc6099171fb7ff275369ea553f39a353ab17a9c5
Sam Trenholme <strenholme.usenet@gmail.com>
2018-11-09 19:09:14 -0800

    nanorg32.c: 543 bytes

--- C/nanorg32.c
+++ C/nanorg32.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #define b(z) for(c=0;c<z;c++)
-uint32_t c,e[40],f[40],g=19,h=13,n[45],i,k;void m(){int c,j=0;b
-(12)f[c+c%3*h]^=e[c+1];b(g){j+=c;i=c*7%g;k=e[i++];k^=e[i%g]|~e[
-(i+1)%g];n[c]=n[c+g]=k>>j%32|k<<(32-j)%32;}for(c=39;c--;f[c+1]=
-f[c])e[c]=n[c]^n[c+1]^n[c+4];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];
-}int main(int p,char**v){char*q=v[--p];for(;;m()){b(3){for(p=0;
-p<4;){f[c*h]^=k=(*q?255&*q:1)<<8*p++;e[16+c]^=k;if(!*q++){b(18)
-m();b(8){i=c;k=e[1+c%2];b(4){printf("%02x",255&k);k>>=8;}c=i;if
-(c%2)m();}return 0&puts("");}}}}}
+uint32_t c,e[42],f[42],g=19,h=13,n[45],i,k;void m(){int c,
+j=0;b(12)f[c+c%3*h]^=e[c+1];b(g){i=c*7%g;k=e[i++];j+=c;k^=
+e[i%g]|~e[(i+1)%g];n[c]=n[c+g]=k>>j%32|k<<-j%32;}for(i=39;
+i--;f[i+1]=f[i])e[i]=n[i]^n[i+1]^n[i+4];b(3)e[c+h]^=f[c*h]
+=f[c*h+h];*e^=1;}int main(int p,char**v){char*q=v[--p];for
+(;;m()){b(3){for(p=0;p<4;){f[c*h]^=k=(*q?255&*q:1)<<8*p++;
+e[c+16]^=k;if(!*q++){b(18)m();b(8){p=c;b(4)printf("%02x",(
+e[1+p%2]>>8*c)&255);if(p%2)m();c=p;}return 0&puts("");}}}}
+}
