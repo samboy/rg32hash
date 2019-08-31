ebeb48bceaf039e0b2fe0da58148813c6913f27b
Sam Trenholme <strenholme.usenet@gmail.com>
2019-08-30 20:40:40 -0700

    nanorg32-vertical.c: Remove -Wall compile-time warning

--- C/nanorg32-vertical.c
+++ C/nanorg32-vertical.c
@@ -5,8 +5,8 @@ uint32_t c,e[42],f[42],g=19,h
 =13,n[45],i,j,k;void m(){j=0;
 b(12)f[c+c%3*h]^=e[c+1];b(g){
 i=c*7%g;k=e[i++];k^=e[i%g]|~e
-[(i+1)%g];n[c]=n[c+g]=k>>(j+=
-c)%32|k<<-j%32;}for(i=h*3;i--
+[(i+1)%g];j=j+c;n[c]=n[c+g]=k
+>>j%32|k<<-j%32;}for(i=39;i--
 ;f[i+1]=f[i])e[i]=n[i]^n[i+1]
 ^n[i+4];b(3)e[c+h]^=f[c*h]=f[
 c*h+h];*e^=1;}int main(int c,
