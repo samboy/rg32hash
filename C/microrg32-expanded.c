41191b74b8e09f2a568797d850eb39d2cd03f269
Sam Trenholme <strenholme.usenet@gmail.com>
2020-03-23 20:59:57 -0700

    microrg32-expanded.c: Comment beltMill()

--- C/microrg32-expanded.c
+++ C/microrg32-expanded.c
@@ -10,27 +10,37 @@ uint32_t c,mill[42],belt[42],g=19,h=13,r,s,n[45],i,k;
 char*q,y;
 
 // This is the cryptographic core of RadioGatun[32], a combination of
-// its belt and mill
+// its belt and mill.  The reference specification for this code is
+// at http://radiogatun.noekeon.org/RadioGatun.pdf
 void beltMill() {
 	int c, j = 0;
-	for(c=0;c<12;c++) {
+
+	// Mill to belt feedforward
+	for(c = 0; c < 12; c++) {
 		belt[c+c%3*h] ^= mill[c+1];
 	}
+
 	for(c=0;c<19;c++) {
-		j = (j + c) % 32;
+		// Non-linearity
 		i = c * 7 % g;
 		k = mill[i++];
 		k ^= mill[i % g] | ~mill[(i + 1) % g];
+
+		// dispersion
+		j = (j + c) % 32;
 		n[c] = k >> j | k << (32 - j) % 32;
 		n[c + g] = n[c];
 	}
-	for(c=39;c--;belt[c+1]=belt[c]) {
-		mill[c] = n[c]^n[c+1]^n[c+4];
+
+	for(c=39;c--;belt[c+1]=belt[c]) { // Belt rotation part one
+		mill[c] = n[c]^n[c+1]^n[c+4]; // diffusion
 	}
-	mill[0] ^= 1;
-	for(c=0;c<3;c++) {
-		belt[c*h] = belt[c*h+h];
-		mill[c+h] ^= belt[c*h];
+
+	mill[0] ^= 1; // asymmetry
+
+	for(c=0;c<3;c++) { 
+		belt[c*h] = belt[c*h+h]; // Belt rotation part two
+		mill[c+h] ^= belt[c*h]; // Belt to mill feedforward
 	}
 }
 
@@ -70,11 +80,11 @@ void generateOutput(char **v) {
 
 int main(int p,char **v){
 	q=getenv("P");
-	if(q&&p>2) {
+	if(q && p>2) {
 		for(;;beltMill()) {
 			for(c=0;c<3;c++) {
 				for(r=0;r<4;r++) {
-					k=(*q?*q&255:1)<<8*r;
+					k = (*q?*q&255:1)<<8*r;
 					belt[c*h] ^= k;
 					mill[16+c] ^= k;
 					if(!*q) {
