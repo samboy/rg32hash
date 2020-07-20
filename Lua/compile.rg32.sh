00b1464f20c3ea51da98762c1b3e50ad6497008d
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-19 17:03:19 -0700

    compile.rg32.sh: Add Mac notes

--- Lua/compile.rg32.sh
+++ Lua/compile.rg32.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+# This is for Linux and Cygwin.
 gcc -shared -o rg32.so rg32.c -llua
 if uname -a | grep CYGWIN > /dev/null ; then
 	ln -s rg32.so rg32.dll
 fi
+
+# Mac users may or may not get things to work with 
+# gcc -bundle -undefined dynamic_lookup -o rg32.so rg32.c -llua
