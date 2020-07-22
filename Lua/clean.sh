0a3b34eb41723bb57744137d1a2e81182562156f
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-22 07:59:54 -0700

    Update compile.rg32.sh (-O2); add clean.sh

---
+++ Lua/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm rg32.so
+rm -f rg32.dll
--- Lua/compile.rg32.sh
+++ Lua/compile.rg32.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is for Linux and Cygwin.
-gcc -shared -o rg32.so rg32.c -llua
+gcc -O2 -shared -o rg32.so rg32.c -llua
 if uname -a | grep CYGWIN > /dev/null ; then
 	ln -s rg32.so rg32.dll
 fi
